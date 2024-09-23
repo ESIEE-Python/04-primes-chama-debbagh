07f26c771da0a02aff24f46b71b8f4fe624decb8
chama-debbagh <chama.debbagh@edu.esiee.fr>
2024-09-23 12:55:28 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+"sqt qui permet de donner la racine carre"
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    "vérifie si un entier est un nombre premier ou pas."
+    if p <= 1 :
+        return False
+    for i in range (2,int(sqrt(p)+1) ):
+        if p % i == 0 :
+            return False
+    return True
 
     # votre code ici
 
-    pass
 
 #### Fonction principale
 
 
 def main():
-
+    "fait quelques appels à la fonction secondaire"
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
